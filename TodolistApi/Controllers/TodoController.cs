dffa864e65e5ce94aa689250ee23395d89622c5f
Татьяна Лавринец <tatianalavrinec@MacBook-Pro-Tatana.local>
2023-06-02 22:51:14 +0300

    first commit

---
+++ TodolistApi/Controllers/TodoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodolistApi.Models;
+
+namespace TodolistApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TodoController : ControllerBase
+{
+    private static ConcurrentDictionary<int, TodoItem> _todos = new ();
+    private static int Count = 0;
+
+    
+
+    [HttpPost]
+    public async Task<IActionResult> AddItem(string name)
+    {
+        TodoItem item = new TodoItem();
+        item.Name = name;
+        item.CreatedAt = DateTime.Now;
+        item.Id = Interlocked.Increment(ref Count);
+
+        if(_todos.TryAdd(item.Id,item))
+        {
+            return Ok(item);
+        }
+
+        return BadRequest();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetItem(int id)
+    {
+        var itemExists = _todos.TryGetValue(id,out var item);
+
+        if (itemExists)
+        {
+            return Ok(item);
+        }
+
+        return NotFound();
+    }
+
+    [HttpDelete]
+    public async Task <IActionResult> Delete(int id)
+    {
+        if (_todos.TryRemove(id, out _) is false)
+            return BadRequest();
+         
+        return Ok("mes");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update()
+    {
+
+
+        return Ok();
+    }
+}
+
---
+++ TodolistApi/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TodolistApi.Models
+{
+	public class TodoItem
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
